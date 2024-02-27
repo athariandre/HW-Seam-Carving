1085dfda0a89bd91340c85e1ca3d69ce682d0ed6
athariandre <athariandre@gmail.com>
2024-02-26 18:49:51 -0600

    UGH

--- functions.cpp
+++ functions.cpp
@@ -48,7 +48,7 @@ void loadImage(string filename, Pixel image[][MAX_HEIGHT], unsigned int& width, 
     for(unsigned int j = 0; j < width; j++){
       inFS >> pr >> pg >> pb;
       if(inFS.fail()){
-        throw std::runtime_error("Invalid color value1" + (string)inFS.);
+        throw std::runtime_error("Invalid color value1");
       }
       if(pr >= 256){
         throw std::runtime_error("Invalid color value2");
