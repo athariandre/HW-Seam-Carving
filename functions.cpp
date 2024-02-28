b9134f718de0a5f03b778827ebc45d20aea5141c
athariandre <athariandre@gmail.com>
2024-02-28 14:43:52 -0600

    idk

--- functions.cpp
+++ functions.cpp
@@ -47,13 +47,23 @@ void loadImage(string filename, Pixel image[][MAX_HEIGHT], unsigned int& width, 
 
   for(unsigned int i = 0; i < height; i++){
     for(unsigned int j = 0; j < width; j++){
-      inFS >> pr >> pg >> pb;
 
-      if(inFS.fail() || pr >= 256 || pb >= 256 || pg >= 256 || pr < 0 || pb < 0 || pg < 0){
+      inFS >> pr;
+      if (inFS.fail() || pr >= 256 || pr < 0) {
         throw std::runtime_error("Invalid color value");
       }
 
-      
+      inFS >> pg;
+      if (inFS.fail() || pg >= 256 || pg < 0) {
+        throw std::runtime_error("Invalid color value");
+      }
+
+      inFS >> pb;
+      if (inFS.fail() || pb >= 256 || pb < 0) {
+        throw std::runtime_error("Invalid color value");
+      }
+
+
       Pixel p = {pr,pg,pb};
       image[j][i] = p;  
     }
